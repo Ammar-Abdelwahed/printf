34d927eaf40c265aefa31adfd6568935bfc943b8
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-04-24 14:04:53 -0700

    update

--- out_stream.c
+++ out_stream.c
@@ -37,7 +37,7 @@ int handle_write_char(char c, char buffer[],
 			return (write(1, &buffer[0], 1) +
 					write(1, &buffer[1023 - i], width - 1));
 		else
-			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+			return (write(1, &buffer[1022 - i], width - 1) +
 					write(1, &buffer[0], 1));
 	}
 
