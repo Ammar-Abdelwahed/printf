0eab15fd7a36189f76251298a5e650f89b427302
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-04-24 13:41:22 -0700

    commit file g_size that have function g_size that Calculates the size to cast the argument

---
+++ g_size.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*g_size - Calculates the size to cast the argument
+*@format: Formatted string in which to print the arguments
+*@i: List of arguments to be printed.
+*Return: Precision.
+*/
+int g_size(const char *format, int *i)
+{
+int c_i = *i + 1;
+int size = 0;
+if (format[c_i] == 'l')
+size = LONG;
+else if (format[c_i] == 'h')
+size = SHORT;
+if (size == 0)
+*i = c_i - 1;
+else
+*i = c_i;
+return (size);
+}
